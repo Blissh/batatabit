7af53f21c9264825821d39379a6255bddeedd255
Blissh <anthony.aka21@gmail.com>
2023-07-06 19:00:12 -0500

    table mobile section done

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const swipeButton = document.getElementById("swipe-button");
+const coinsTable = document.querySelector("#main-container__tables-coins");
+const comissionsTable = document.querySelector(
+  "#main-container__tables-comissions"
+);
+const spanIcon = swipeButton.querySelector("span");
+
+swipeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (comissionsTable.classList.toggle("hidden")) {
+    coinsTable.classList.remove("hidden");
+    swipeButton.classList.replace("left-position","right-position");
+    spanIcon.classList.replace("arrow-left","arrow-right");
+  } else {
+    coinsTable.classList.add("hidden");
+    swipeButton.classList.replace("right-position","left-position");
+    spanIcon.classList.replace("arrow-right","arrow-left");
+  }
+});
